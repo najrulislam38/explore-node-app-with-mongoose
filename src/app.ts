f63fe2e6629c8776db0e7b89a1ad4f34ba57c486
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-06-16 22:21:21 +0600

    create update and delete method and set options for schema

--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,29 @@ const app: Application = express();
 // Middleware
 app.use(express.json());
 
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true, trim: true },
-  content: { type: String, default: "" },
-  category: {
-    type: String,
-    enum: ["personal", "work", "study", "others"],
-    default: "personal",
+const noteSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: "" },
+    category: {
+      type: String,
+      enum: ["personal", "work", "study", "others"],
+      default: "personal",
+    },
+    pinned: {
+      type: Boolean,
+      default: false,
+    },
+    tags: {
+      label: { type: String, required: true },
+      color: { type: String, default: "gray" },
+    },
   },
-  pinned: {
-    type: Boolean,
-    default: false,
-  },
-  tags: {
-    label: { type: String, required: true },
-    color: { type: String, default: "gray" },
-  },
-});
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const Note = model("Note", noteSchema);
 
@@ -56,6 +62,64 @@ app.get("/notes", async (req: Request, res: Response) => {
     notes,
   });
 });
+
+app.get("/notes/:noteId", async (req: Request, res: Response) => {
+  // get single data using Id
+  const noteId = req.params.noteId;
+  const note = await Note.findById(noteId);
+
+  // get single data using any field.
+  const singleNote = await Note.findOne({ title: "Learning Mongoose" });
+
+  res.status(200).json({
+    success: true,
+    message: "note show successfully",
+    note: singleNote,
+  });
+});
+
+app.patch("/notes/update-note/:noteId", async (req: Request, res: Response) => {
+  const noteId = req.params.noteId;
+  const updateNote = req.body;
+
+  // update single data approach - 1 (Most use case)
+  const note = await Note.findByIdAndUpdate(noteId, updateNote, { new: true });
+
+  // update single data approach - 2
+  // const note = await Note.findOneAndUpdate({ _id: noteId }, updateNote, {
+  //   new: true,
+  // });
+
+  // update single data approach - 3 low uses
+  // const note = await Note.updateOne({ _id: noteId }, updateNote, {
+  //   new: true,
+  // });
+
+  res.status(200).json({
+    success: true,
+    message: "note update successfully",
+    note,
+  });
+});
+
+app.delete(
+  "/notes/delete-note/:noteId",
+  async (req: Request, res: Response) => {
+    const noteId = req.params.noteId;
+    const body = req.body;
+
+    // use 3 way to delete data.
+    const note = await Note.findByIdAndDelete(noteId);
+    const note2 = await Note.findOneAndDelete({ _id: noteId });
+    const note3 = await Note.deleteOne({ _id: noteId });
+
+    res.status(200).json({
+      success: true,
+      message: "data deleted successfully",
+      note3,
+    });
+  }
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the server.");
